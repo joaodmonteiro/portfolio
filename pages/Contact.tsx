47ff0e25c435ca1e5a609de0ecc68e1ccfe17bb7
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-01 16:26:56 +0100

    contact page

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,7 +1,49 @@
 import { NextPage } from "next";
+import styles from "../styles/Contact.module.scss";
 
 const Contact: NextPage = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Get in touch!</h1>
+      <div className={styles.links_container}>
+        <div className={styles.link_item}>
+          <a href="" target="_blank" rel="noreferrer">
+            <div className={styles.link}>
+              <img src="/images/arrow.svg" alt="" />
+              <div>Email</div>
+            </div>
+          </a>
+          <div>hello@joaodmonteiro.com</div>
+        </div>
+
+        <div className={styles.link_item}>
+          <a
+            href="https://twitter.com/joaodmonteiro"
+            target="_blank"
+            rel="noreferrer">
+            <div className={styles.link}>
+              <img src="/images/arrow.svg" alt="" />
+              <div>Twitter</div>
+            </div>
+          </a>
+          <div>@joaodmonteiro</div>
+        </div>
+
+        <div className={styles.link_item}>
+          <a
+            href="https://www.linkedin.com/in/joaodmonteiro/"
+            target="_blank"
+            rel="noreferrer">
+            <div className={styles.link}>
+              <img src="/images/arrow.svg" alt="" />
+              <div>LinkedIn</div>
+            </div>
+          </a>
+          <div>@joaodmonteiro</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
