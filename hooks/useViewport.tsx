8ad5e783e55f610d3cebfee03e31da92e6fdf2f9
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-03 19:53:13 +0100

    hook to get viewport size

---
+++ hooks/useViewport.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useViewport = () => {
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return { width, height };
+};
+
+export default useViewport;
