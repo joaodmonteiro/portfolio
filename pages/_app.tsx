2b732847441d663a7c82e84d3dda6bc6a0f2bdbf
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-05 14:37:12 +0100

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Nav from "../components/Nav";
 import ScrollContainer from "../components/ScrollContainer";
 import { useState } from "react";
 import Menu from "../components/Menu";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,11 +23,35 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <div>
-      <Nav handleMenuClick={handleMenuClick} />
-      <Component {...pageProps} />
-      {menuOpen && <Menu closeMenu={closeMenu} />}
-    </div>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div>
+        <Nav handleMenuClick={handleMenuClick} />
+        <Component {...pageProps} />
+        {menuOpen && <Menu closeMenu={closeMenu} />}
+      </div>
+    </>
   );
 }
 
