feb4da7e1e4d261369c246e02242bfa6f01bff18
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-13 12:28:12 +0100

    footer fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,8 +58,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, 
-      shrink-to-fit=no"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
       <div>
